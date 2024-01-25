14f6bbe23bb245ebdb6b00e18656f4689d63cf57
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-01-25 09:47:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pg9pvgd.mongodb.net/?retryWrites=true&w=majority`;
+
+// middleware
+app.use(express());
+app.use(cors());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// home routes
+app.get("/", (req, res) => {
+  res.send("server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`server is running successfully at http://${port}`);
+});
